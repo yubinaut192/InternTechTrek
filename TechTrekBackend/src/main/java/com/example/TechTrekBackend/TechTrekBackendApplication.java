05a42e54c864165ca580d44bad6a78305a785382
Anand <arulmozhivarman.anand@go-yubi.com>
2024-03-08 14:45:50 +0530

    added spring initial files

---
+++ TechTrekBackend/src/main/java/com/example/TechTrekBackend/TechTrekBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechTrekBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTrekBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTrekBackendApplication.class, args);
+	}
+
+}
---
+++ TechTrekBackend/src/test/java/com/example/TechTrekBackend/TechTrekBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechTrekBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTrekBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
